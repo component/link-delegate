0f9e9b5248abc900b103eec657e331bda767e8b3
TJ Holowaychuk <tj@vision-media.ca>
2013-05-02 12:41:25 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,36 @@
+
+# link-delegate
+
+  Anchor tag click delegation / negotiation for client-side routing. This component
+  makes it easy to implement application-wide push-state link handling without
+  manually performing a router dispatch.
+
+## Installation
+
+    $ component install component/link-delegate
+
+## Example
+
+  The callback is invoked with an `Event` when the link:
+
+  - is not x-domain
+  - is the primary "mouse" button
+  - does not have `e.defaultPrevented` (to opt-out)
+  - does not specify a `target`
+  - does not have the meta, ctrl, or shift key pressed
+
+```js
+var link = require('link-delegate');
+
+link(function(e){
+  e.preventDefault();
+
+  // perform your routing here
+  console.log(e.target.href);
+});
+```
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,60 @@
+
+/**
+ * Module dependencies.
+ */
+
+var delegate = require('delegate');
+var url = require('url');
+
+/**
+ * Handle link delegation on `el` or the document,
+ * and invoke `fn(e)` when clickable.
+ *
+ * @param {Element|Function} el or fn
+ * @param {Function} [fn]
+ * @api public
+ */
+
+module.exports = function(el, fn){
+  // default to document
+  if ('function' == typeof el) {
+    fn = el;
+    el = document;
+  }
+
+  delegate.bind(el, 'a', 'click', function(e){
+    if (clickable(e)) fn(e);
+  });
+};
+
+/**
+ * Check if `e` is clickable.
+ */
+
+function clickable(e) {
+  if (1 != which(e)) return;
+  if (e.metaKey || e.ctrlKey || e.shiftKey) return;
+  if (e.defaultPrevented) return;
+
+  // target
+  var el = e.target;
+
+  // check target
+  if (el.target) return;
+
+  // x-origin
+  if (url.isCrossDomain(el.href)) return;
+
+  return true;
+}
+
+/**
+ * Event button.
+ */
+
+function which(e) {
+  e = e || window.event;
+  return null == e.which
+    ? e.button
+    : e.which;
+}
